8a27ab913acb17814b062716465f760dd5a23ef0
apzzoe <apazazoe8@gmail.com>
2026-05-05 22:13:56 -0300

    Agregado ejercicio20.html, ejercicio20.css y ejercicio20.js abajo de los import de la asistencia 30/04 - Zelaya Apaza Zoe

--- src/js/ejercicio20.js
+++ src/js/ejercicio20.js
@@ -1,5 +1,22 @@
-import { servivioMelinaLeaño } from "../services/serviceMelinaLeaño.js";
-import { servicioLujanButron } from "../services/serviceLujanButron.js";
-import { servicioZelayaApazaZoe } from "../services/serviceZelayaApazaZoe.js";
-import { serviceJosefinaRios } from "../services/serviceJosefinaRios.js";
-import { serviceLuisanaBarbaraVillena} from "../services/serviceLuisanaBarbaraVillena.js"
+// import { servivioMelinaLeaño } from "../services/serviceMelinaLeaño.js";
+// import { servicioLujanButron } from "../services/serviceLujanButron.js";
+// import { servicioZelayaApazaZoe } from "../services/serviceZelayaApazaZoe.js";
+// import { serviceJosefinaRios } from "../services/serviceJosefinaRios.js";
+// import { serviceLuisanaBarbaraVillena} from "../services/serviceLuisanaBarbaraVillena.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+const boton = document.querySelector("#btn");
+
+boton.addEventListener("click", () => {
+
+    const nombre = document.querySelector("#nombre").value;
+    const apellido = document.querySelector("#apellido").value;
+    const lu = document.querySelector("#lu").value;
+
+    alert(`Los datos ingresados son:
+Nombre: ${nombre}
+Apellido: ${apellido}
+Libreta Universitaria: ${lu}`);
+
+});
+});
